3ed48fd0651008379943c8f52d14577f0d54ebe3
Olivier Mangin <aicel@make-it.fr>
2019-08-01 01:22:05 +0200

    add App.jsx

---
+++ imports/startup/client/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { ToastContainer } from "react-toastify";
+
+import Logged     from '/imports/ui/components/Routes/Logged';
+import NotLogged  from '/imports/ui/components/Routes/NotLogged';
+import SideBar    from '/imports/ui/components/SideBar';
+
+import Landing    from '/imports/ui/modules/Landing';
+import Rooms      from '/imports/ui/modules/Rooms';
+import Users      from '/imports/ui/modules/Users';
+import {
+  Connexion,
+  Inscription,
+  Missing,
+  Settings,
+  Verify
+} from '/imports/ui/modules/Account';
+
+const App = () => (
+  <Router>
+    <SideBar />
+    <Switch>
+      <Logged       path="/rooms/:roomId?"  component={Rooms}        />
+      <Logged       path="/users/:userId?"  component={Users}        />
+      <NotLogged    path="/signin"          component={Connexion}    />
+      <NotLogged    path="/signup"          component={Inscription}  />
+      <Logged       path="/missing"         component={Missing}      />
+      <Logged       path="/settings"        component={Settings}     />
+      <Logged       path="/verify/:token"   component={Verify}       />
+      <NotLogged    path="/"                component={Landing}      />
+    </Switch>
+    <ToastContainer />
+  </Router>
+);
+
+export default App;
